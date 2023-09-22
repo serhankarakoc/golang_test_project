852bf6dd6763e61f088ab1a36c2a7f835176b1b5
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2023-09-22 11:41:09 +0300

    Update email_notifier.go

--- notifiers/email_notifier.go
+++ notifiers/email_notifier.go
@@ -6,5 +6,5 @@ type EmailNotifier struct{}
 
 func (n EmailNotifier) Send(content map[string]string) {
 	fmt.Printf("Sending Email to %s: %s - %s\n", content["email"], content["title"], content["content"])
-	// Add email sending logic here
+	// Eposta gönderim işlemi
 }
