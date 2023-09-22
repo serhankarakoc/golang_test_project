0e045e5de4970738180e69b4b8748d881ce4c184
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2023-09-22 11:42:03 +0300

    Update sms_notifier.go

--- notifiers/sms_notifier.go
+++ notifiers/sms_notifier.go
@@ -6,5 +6,5 @@ type SMSNotifier struct{}
 
 func (n SMSNotifier) Send(content map[string]string) {
 	fmt.Printf("Sending SMS to %s: %s\n", content["phone"], content["content"])
-	// Add SMS sending logic here
+	// Sms gönderim işlemi
 }
